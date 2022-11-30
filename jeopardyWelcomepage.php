a9a5ee102dac7b54581354171277e8028d138f04
ChaseBeard <119538085+ChaseBeard@users.noreply.github.com>
2022-11-30 16:10:28 -0600

    Update jeopardyWelcomepage.php

--- jeopardyWelcomepage.php
+++ jeopardyWelcomepage.php
@@ -1,5 +1,36 @@
 <!DOCTYPE html>
 
+<!--style needed, implement css-->
 <head>
-    <p>hello my friend<p>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@700&display=swap" rel="stylesheet">
+
+    <title>CSJeopardy! Menu</title>
 </head>
+
+<body>
+    <div class="col-ls-7 pt-6 px-6">
+    <h1>CSJeopardy!</h1>
+        <form method="post">
+            <div class="form-row mb-5"> 
+                <input type="button" onclick="window.location.href='playgame.php';" 
+                class="btn btn-warning btn-lg" value="PLAY GAME"/>
+            
+            </div>
+            <div class="form-row mb-6">
+                <input type="button" onclick="window.location.href='leaderboard.php';" 
+                class="btn btn-warning btn-lg" value="LEADERBOARD"/>
+
+            </div>  
+            <div class="form-row mb-7">
+                <input type="button" onclick="window.location.href='Login.php';" 
+                class="btn btn-warning btn-lg" value="QUIT"/>
+               
+            </div>
+        </form>
+    </div>
+
+</body>
