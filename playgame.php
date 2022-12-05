fb1842774dd0c88145643980de03689115de7b66
tok2556 <99053216+tok2556@users.noreply.github.com>
2022-12-05 17:22:10 -0600

    Update playgame.php

--- playgame.php
+++ playgame.php
@@ -1,5 +1,25 @@
 <!DOCTYPE html>
 
-<head>
-    <p>hello my friend<p>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CSJeopardy! Play</title>
+    </head>
+
+    <body>
+        <div class="col-ls-6 pt-5 px-5">
+            <h1>CSJeopardy!</h2>
+            <p>Select a category</p>
+            <table class='striped white'>
+                <thead>
+                <tr>
+                    <th>Category1</th>
+                    <td>
+                        <input type="button" onclick="window.location.href='question.php';" class="btn btn-warning btn-lg" value="100"/>
+                    </td>
+                    <th>Category2</th>
+                    <th>Category3</th>
+                    <th>Category4</th>
+                    <th>Category5</th>
+                </tr>
+                </thead>
