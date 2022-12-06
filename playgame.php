e54dd6df54162e8fdd5047f00b1e13e838241872
ChaseBeard <119538085+ChaseBeard@users.noreply.github.com>
2022-12-05 19:35:38 -0600

    Update playgame.php

--- playgame.php
+++ playgame.php
@@ -1,25 +1,76 @@
 <!DOCTYPE html>
+<html>
+<head>
+	<title>CSJeopardy!</title>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>CSJeopardy! Play</title>
-    </head>
 
-    <body>
-        <div class="col-ls-6 pt-5 px-5">
-            <h1>CSJeopardy!</h2>
-            <p>Select a category</p>
-            <table class='striped white'>
-                <thead>
-                <tr>
-                    <th>Category1</th>
-                    <td>
-                        <input type="button" onclick="window.location.href='question.php';" class="btn btn-warning btn-lg" value="100"/>
-                    </td>
-                    <th>Category2</th>
-                    <th>Category3</th>
-                    <th>Category4</th>
-                    <th>Category5</th>
-                </tr>
-                </thead>
+	<meta name="viewport" content="width=device-width" initial-scale=1.0">
+
+
+</head>
+<body>
+
+	<div class="wrapper">
+			
+		<div class="container">
+
+			<div class="header">
+				<h1>CSJeopardy!</h1>
+			</div>
+				
+			<div class="stats-bar">
+					
+			</div>
+
+			<div class="welcome-screen"></div>
+
+			<div class="restart-screen"></div>
+
+			<div class="game-area">
+
+				<div id="category"></div>
+
+				<div id="question-value"></div>
+
+				<div id="question"></div>
+
+				<div id="response-buttons">
+					
+					<ul class="response-choices">
+
+					</ul>
+
+				</div>
+
+				<div id="timer">
+					
+				</div>
+
+				<div id="dialog-correct-next-question"></div>
+				<div id="dialog-incorrect-next-question"></div>
+				<div id="dialog-times-up-next-question"></div>
+
+
+			</div>
+
+			<div class="clear"></div>
+			
+		</div>
+
+	</div>
+
+
+	<!-- JavaScript -->
+	<script type="text/javascript" src="assets/javascript/game.js"></script>
+	<script type="text/javascript" src="assets/javascript/view.js"></script>
+	<script type="text/javascript" src="assets/javascript/model.js"></script>
+
+
+	<!-- Link to jQuery CDN -->
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+	<!-- Link to jQuery UI CDN -->
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+	
+</body>
+</html>
