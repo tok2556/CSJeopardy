6554ff999bdf3ddffdf0af2cc80e8843fa772f89
ChaseBeard <119538085+ChaseBeard@users.noreply.github.com>
2022-12-07 23:25:14 -0600

    Update jeopardyWelcomepage.php

--- jeopardyWelcomepage.php
+++ jeopardyWelcomepage.php
@@ -3,7 +3,7 @@
     session_start();
 
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-        echo "Welcome to CSJeopardy, " . htmlspecialchars($_SESSION['username']) . "!";
+        echo "<h5>Welcome to CSJeopardy, " . htmlspecialchars($_SESSION['username']) . "!</h5>";
     } else {
         header('Location: Login.php');
     }
@@ -24,20 +24,31 @@
 
     <title>CSJeopardy! Menu</title>
 </head>
-
+<style>
+    html, body, table {margin: 0; padding: 0; background-color: #0000ff; color: #ffffff;}
+    h5 {text-align: left; font-family default;}
+    body {text-align: center;}
+    h1 {text-align: center; color:#ffff33; font-size: 75px; text-shadow: black 3px 3px; font-weight: bold; font-family:"HelveticaNeue-CondensedBold";}
+    input {background-color: White; border-radius: 6px; text-align: center; font-size: 25px; transition-duration: 0.1s;}
+    input:hover {background-color: #ffff33; color: black;}
+</style>
 <body>
     <div class="col-ls-7 pt-6 px-6">
     <h1>CSJeopardy!</h1>
         <form method="post">
             <div class="form-row mb-5"> 
-                <input type="button" onclick="window.location.href='playgame.php';" 
+                <input type="button" onclick="window.location.href='play.php';" 
                 class="btn btn-warning btn-lg" value="PLAY GAME"/>
-            
+            <br>
+
+            </br>
             </div>
             <div class="form-row mb-6">
                 <input type="button" onclick="window.location.href='leaderboard.php';" 
                 class="btn btn-warning btn-lg" value="LEADERBOARD"/>
+            <br>
 
+            </br>
             </div>  
             <div class="form-row mb-7">
                 <input type="button" onclick="window.location.href='logout.php';" 
