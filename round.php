c22f25fab358eee290f7d03acd7d110cc6079608
ChaseBeard <119538085+ChaseBeard@users.noreply.github.com>
2022-12-07 23:18:32 -0600

    Create round.php

---
+++ round.php
@@ -0,0 +1,132 @@
+<?php return [
+	'pointScale' => 100,
+	'categories' => [
+		[
+			'name' => 'Database',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'xx',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'xx',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'xx',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'xx',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+	]
+];
+
+?>
